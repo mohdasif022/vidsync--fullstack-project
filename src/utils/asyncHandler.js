86e65c9a524142e2d4e3f63e7e72134c6ecf7375
Mohammad Asif <mohdasif7724@gmail.com>
2024-06-02 17:14:34 +0530

    utils code

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,25 @@
+const asynHandler = (requestHandler) => {
+     return (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err))
+    }
+}
+
+
+export { asynHandler }
+
+
+
+
+
+// try catch method
+
+// const asynHandler = (fn) =>  async(req, res, next) => {
+//     try {
+//         await fn(req, res, next)
+//     } catch (error) {
+//         res.status(error.code || 500).json({
+//             success: false,
+//             message: error.message
+//         })
+//     }
+// }
