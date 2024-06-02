b9a748739bc06c32ce9d1bcd1b6804fb0d024108
Mohammad Asif <mohdasif7724@gmail.com>
2024-06-02 16:20:34 +0530

    connect to database

---
+++ src/index.js
@@ -0,0 +1,15 @@
+// require('dotenv').config({path: './env'})
+import dotenv from 'dotenv';
+import connectDB from "./db/index.js";
+
+
+dotenv.config({
+    path: './env'
+})
+
+
+
+connectDB();
+
+
+
