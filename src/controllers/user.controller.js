7eef0ec3a9ac0c3f8b243404db2eab4794924c6b
Mohammad Asif <mohdasif7724@gmail.com>
2024-06-04 12:13:41 +0530

    user register route completed

---
+++ src/controllers/user.controller.js
@@ -0,0 +1,11 @@
+import { asynHandler } from "../utils/asyncHandler.js";
+
+const registerUser = asynHandler( async (req, res) => {
+    res.status(200).json({
+        message: "ok"
+    })
+})
+
+
+
+export {registerUser}
